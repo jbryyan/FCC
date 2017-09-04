081bbdf58e30fcd29a8b7ac4a1105cf66ffa1e0e
Bryan <jbryyan@gmail.com>
2017-09-04 13:27:02 -0700

    Finished layout. Need to add minor fixes.

--- docs/WikiViewer/main.js
+++ docs/WikiViewer/main.js
@@ -1,6 +1,7 @@
 
 function changeStyle(){
   //Variables used to change html element styles
+
   var wikiBox = document.getElementById("wikiBox");
   var searchInput = document.getElementById("searchInput");
   var searchButton = document.getElementById("searchButton");
@@ -10,27 +11,37 @@ function changeStyle(){
   wikiBox.style.margin = 0;
   wikiBox.style.height = "85px";
   wikiBox.style.position = "fixed";
-  randomButton.style.display = "inline-block";
-  searchInput.style.height = (30/85)*100 + "%"; /* 25px / wikiBox.clientHeight */
+
+  //searchInput.style.height = (30/85)*100 + "%";  //30px / wikiBox.clientHeight
   searchInput.style.margin = 0;
-  searchButton.style.height = (35/85)*100 + "%"; /* 30px / wikiBox.clientHeight */
-  randomButton.style.height = (35/85)*100 + "%";
+  //searchButton.style.height = (35/85)*100 + "%"; //35px / wikiBox.clientHeight
+  //randomButton.style.height = (35/85)*100 + "%";
+
+/*
+  var wikiBox = document.getElementById("wikiBox");
+  var formBox = document.getElementById("formBox");
+
+  wikiBox.style.margin = 0;
+  wikiBox.style.height = "85px";
+  wikiBox.style.position = "fixed";
+  formBox.style.margin = "1222px;"*/
 
 }
 
-//Function resets back to original landing page.
+//Function resets page back to original landing page.
 function resetStyle(){
   document.querySelector("#wikiBox").removeAttribute("style");
   document.getElementById("searchInput").removeAttribute("style");
   document.getElementById("searchButton").removeAttribute("style");
   document.getElementById("randomButton").removeAttribute("style");
+
   if(document.getElementById("wikiData")){
     document.getElementById("wikiData").remove();
   }
   document.getElementById("wikiScript").remove();
 }
 
-//Dynamically setting top margin for new div that will store wiki info
+//Dynamically setting top margin for new div that will post wiki info to page.
 function createDiv(){
     var newDiv = document.createElement("div");
     var topMargin;
@@ -39,21 +50,77 @@ function createDiv(){
     newDiv.className = "wikiData";
     document.getElementsByTagName("body")[0].appendChild(newDiv);
     topMargin = document.getElementById("wikiBox").clientHeight;
-    document.getElementById("wikiData").style.marginTop = topMargin +"px";
+    document.getElementById("wikiData").style.marginTop = topMargin + 20 +"px";
+}
+
+//Function used to populate wikiData with wikipedia results.
+function populateDiv(t, c, l){
+  var title = t;
+  var content = c;
+  var link = l;
+
+  console.log(title);
+  for(i = 0; i < title.length; i++){
+    var wikiLink = document.createElement("a");
+    var wikiPar = document.createElement("p");
+    //Setting up link to the wiki article
+    wikiLink.className = "searchLink";
+    wikiLink.href = link[i];
+    wikiLink.textContent = title[i];
+    //Setting up the wiki info retrieved
+    wikiPar.className = "searchInfo";
+    wikiPar.textContent = content[i];
+    document.getElementById("wikiData").appendChild(wikiLink);
+    document.getElementById("wikiData").appendChild(wikiPar);
+  }
+}
+
+//Function used to depopulate wikiData for the next search.
+function depopulateDiv(){
+  var parent = document.getElementById("wikiData");
+  while (parent.firstChild){
+    parent.removeChild(parent.firstChild);
+  }
 }
 
 //Wiki API response
 function readResponse(response){
-  createDiv();
-  var newEle = document.createElement("p");
 
+  //Breaking up API response.
+  var title = response[1];
+  var content = response[2];
+  var link = response[3];
+  console.log(title);
+  //Checking to see if API returned anything.
+  if(title.length == 0){
+    var wikiPar = document.createElement("p");
+    wikiPar.className = "errorInfo";
+    wikiPar.textContent = "Sorry, search term not found. Check spelling and try again."
+    document.getElementById("wikiData").appendChild(wikiPar);
+    return;
+  }
+
+  //Append new children only if in landing page else depopulate and populate with new search results.
+  if(document.getElementById("wikiData").children.length == 0){
+    populateDiv(title, content, link);
+  }
+  else{
+    depopulateDiv();
+    populateDiv(title, content, link);
+  }
 
 }
 
-
+//When search button is clicked..
 function searchWiki(){
-  //Moving search div up to the top of the page.
-  changeStyle();
+
+
+
+  //If the div for the wiki info is not set, it means it's still in landing page.
+  if(!document.getElementById("wikiData")){
+    changeStyle();  //Move landing page div to the top.
+    createDiv();  //Create a wikiData div to post the search results. Total of 10 results.
+  }
 
   //Variables for api request
   //https://en.wikipedia.org/w/api.php?action=opensearch&search=api&limit=10&namespace=0&format=jsonfm
@@ -61,7 +128,7 @@ function searchWiki(){
   var format = "&format=json";
   var action = "&action=opensearch";
   var limit = "&limit=10"
-  var search = "&search=cookies";
+  var search = "&search=" + document.getElementById("searchInput").value;
   var data = "&callback=readResponse";
   var src = "https://en.wikipedia.org/w/api.php?" + format + action + limit + search + data;
 
@@ -70,5 +137,4 @@ function searchWiki(){
   script.src = src;
   script.id = "wikiScript";
   document.body.appendChild(script);
-
 }
