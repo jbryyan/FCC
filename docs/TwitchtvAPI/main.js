83ca04005e899944586ca15aa0ad4fa40be28686
Bryan <jbryyan@gmail.com>
2017-09-07 20:01:25 -0700

    Finished. Come back to this and edit the fonts.

--- docs/TwitchtvAPI/main.js
+++ docs/TwitchtvAPI/main.js
@@ -6,7 +6,6 @@ var defaultValue = 0; //Used to distinguish default streamer values or not in fu
 //Function used to check if streamer exists on TwitchTV
 //The "users" twitch tv endpoint will return valid values if user exists.
 function searchTwitch(){
-  console.log("In search function.");
   var streamer = document.getElementById("inputBox");
   var xhr = new XMLHttpRequest;
   var method = "GET";
@@ -16,7 +15,6 @@ function searchTwitch(){
   xhr.open(method, url, async);
   xhr.onload = function(){
     var data = JSON.parse(xhr.responseText);
-    console.log(data);
     if(data.status == "404" || data.status == "422"){
       alert("Streamer does not extist. Try another.")
       streamer.value = "";
@@ -31,7 +29,39 @@ function searchTwitch(){
 
 //Will hide divs that the user did not search for.
 function searchStreamer(){
-  
+  var streamerName = document.getElementById("inputBar").value;
+  var mainDiv = document.getElementById("mainContainer");
+  var myAnchors = mainDiv.getElementsByTagName("a");
+
+  var index;  //Used to store index of streamer, if found.
+
+  //Looping through anchors on page to search for the streamer name.
+  for(var i = 0; i < myAnchors.length; i++){
+    //Searching the inner html text for streamer.
+    if(myAnchors[i].innerHTML == streamerName){
+      index = i;  //Storing index of streamer in index.
+      break;
+    }
+  }
+  //If streamer exists on list, hide the others but the streamer.
+  //Else, nothing happens. Alert user.
+  if(index){
+    //Grabbing every element with class name "wrapper"
+    var wrappers = document.getElementsByClassName("wrapper");
+    //Looping through each wrapper element and checking
+    //Against index of streamer found
+    for(var i = 0; i < wrappers.length; i++){
+      if(i != index){
+        wrappers[i].style.display = "none"; //Hides streamer from list.
+      }
+    }
+    //Uncheck all radio button to display all again once clicked.
+    document.getElementById("allRadio").checked = false;
+  }else{
+    alert("Not on list. Try adding them.");
+  }
+  //Clear Search Field
+  document.getElementById("inputBar").value = "";
 }
 //This function is solely used to search streamers that are submitted by the user.
 function getJSON2(streamer){
@@ -47,7 +77,7 @@ function getJSON2(streamer){
     if(xhr.readyState == 4 && xhr.status == 200 ){
         //Variable for API response.
         var streamData = JSON.parse(xhr.responseText);
-        console.log(streamData);
+
         //If valid, continue to change HTML with response
         //Else, user is offline.
         if(streamData.stream){
@@ -63,7 +93,6 @@ function getJSON2(streamer){
             document.getElementById("searchBar").style.display = "block";
           }
           defaultValue = 0;
-          console.log(streamerName + "sr");
           offlineStreamer(streamerName);
         }
     }else{
@@ -123,9 +152,9 @@ function defaultStreamer(streamData, endPoint){
 
   //Changed background color to online. (green)
   if(endPoint == "channels"){
-    document.getElementById("preLoad").style.backgroundColor = "#d3d3d3";
+    document.getElementById("wrapper0").style.backgroundColor = "grey";
   }else{
-    document.getElementById("preLoad").style.backgroundColor = "#3CB371";
+    document.getElementById("wrapper0").style.backgroundColor = "#dff0d8";
   }
 
   //Appends streamer logo.
@@ -133,6 +162,7 @@ function defaultStreamer(streamData, endPoint){
   var linkContainer = document.getElementById("titleContainer0");
 
   a.href = url;
+  a.target = "_blank";
   a.id = "title0";
   a.innerHTML = title;
   linkContainer.appendChild(a);
@@ -194,38 +224,56 @@ function createDivs(streamData, endPoint){
   var url = data.url;
   var title = data.display_name;
 
-  //Overall hierarchy: Main Container[Sub Container, Sub container, Sub container, Sub container]
+  //Overall hierarchy:
+  //Wrapper[Main Container[Sub Container, Sub container, Sub container, Sub container],
+  //Video[sub, sub]]
+
   /*Main container along with container cells*/
   var divMain = document.getElementById("mainContainer");
+  var wrapper = document.createElement("div");
   var divContainer = document.createElement("div");
   var divImg = document.createElement("div");
   var divTitle = document.createElement("div");
   var divDesc = document.createElement("div");
   var divExit = document.createElement("div");
+  var divVideo= document.createElement("div");
 
   /*Individual elements in each container*/
   var image = document.createElement("img");
   var anchor = document.createElement("a");
   var para = document.createElement("p");
   var btn = document.createElement("button");
+  var caret = document.createElement("i");
 
   /*Nested div class names and id's*/
   divContainer.className = "streamContainer streamMod";
   divContainer.id = "divCont" + num;
+  wrapper.className = "wrapper";
+  wrapper.id = "wrapper" + num;
+
   if(endPoint == "channels"){
-    divContainer.style.backgroundColor = "#d3d3d3";
+    wrapper.style.backgroundColor = "grey"; //Offline
+    alert("offline");
   }else{
-    divContainer.style.backgroundColor = "#3CB371";
+    wrapper.style.backgroundColor = "#dff0d8"; //Online
+    alert("Online");
   }
+
+  //Adding image div info.
   divImg.className = "imageContainer tableMod";
   divImg.id = "imgContainer" + num;
+  //Adding title div info.
   divTitle.className = "titleContainer tableMod";
   divTitle.id = "titleContainer" + num;
+  //Adding status div info
   divDesc.className = "descContainer tableMod";
   divExit.className = "exitDiv";
-
+  //Adding exit button div info
   btn.className = "exitBtn";
   btn.id= "btn" + num;
+  //Adding video div info
+  divVideo.className = "videoContainer";
+  divVideo.id = "showVideo" + num;
 
   /*Nested element classes and id's*/
   image.className = "image";
@@ -233,6 +281,8 @@ function createDivs(streamData, endPoint){
   anchor.id = "title" + num;
   para.className = "text";
   para.id = "desc" + num;
+  caret.className = "fa fa-caret-down fa-lg aria-hidden='true'";
+  caret.id = "caret" + num;
 
   //Appending appropriate image for null logo response.
   if(logo === null){
@@ -244,11 +294,14 @@ function createDivs(streamData, endPoint){
   /*Adding values to the most inner elements*/
   anchor.href = url;     //Appending href="streamer url"
   anchor.innerHTML = title;
+  anchor.target = "_blank";
   para.innerHTML = status;
   btn.innerHTML = "x";
 
   //Putting it all together.
-  divMain.appendChild(divContainer);
+  divMain.appendChild(wrapper);
+  wrapper.appendChild(divContainer);
+  wrapper.appendChild(divVideo);
   divContainer.appendChild(divImg);
   divImg.appendChild(image);
   divContainer.appendChild(divTitle);
@@ -257,50 +310,133 @@ function createDivs(streamData, endPoint){
   divDesc.appendChild(para);
   divContainer.appendChild(divExit);
   divExit.appendChild(btn);
+  divVideo.appendChild(caret);
   document.getElementById("inputBox").value = "";
+}
+
+function showVideo(parentId){
+  var numID = parentId.match(/\d+/)[0];
+  var divVideo = document.getElementById(parentId);
+  //var titleDiv = document.getElementById("title" + num);
+  var divTitle = "title" + numID;
+  var twitchUser = document.getElementById(divTitle).innerHTML;
+  var url = "http://player.twitch.tv/?channel=" + twitchUser + "&muted=true&autoplay=false";
+  var iFrame = document.createElement("iframe");
+  iFrame.style.display = "block";
+  iFrame.style.width = "100%";
+  iFrame.src = url;
+  iFrame.height = "500";
+  iFrame.width = document.getElementById("mainContainer").clientWidth;
+  iFrame.frameborder = "0"
+  iFrame.scrolling = "no";
+  iFrame.allowfullscreen = "true";
+  iFrame.id = "iframe" + numID;
+  iFrame.backgroundColor = "white;"
+  divVideo.appendChild(iFrame);
+  //After appending the video, turn the carrot upwards.
+  console.log(num);
+  document.getElementById("caret" + parentId.match(/\d+/)[0]).className = ("fa fa-caret-up fa-lg aria-hidden='true'");
+}
+
+function deleteVideo(iframe){
+  document.getElementById(iframe).remove();
+  console.log(iframe);
+  var numID = iframe.match(/\d+/)[0];
+  document.getElementById("caret" + numID).className = ("fa fa-caret-down fa-lg aria-hidden='true'");
+
 }
 
 //Used to change the HTML page layout upon pressing a radio button.
 function changeLayout(){
   //Checking to see what radio button was pressed
   var buttonClicked = document.querySelector('input[name="rad"]:checked').value;
+  //Online, offline or all buttons were clicked. Appropriate functions called.
   if(buttonClicked == "Online"){
-    alert("Online");
+    onlineRadio("Online");
   }
   else if(buttonClicked == "Offline"){
-    alert("Offline");
-  }else{
-    alert("All");
+    offlineRadio("Offline");
+  }else if(buttonClicked == "All"){
+    allRadio("All");
   }
 }
 
 function eventListener(){
   //Event delegation for onclick div elements.
   document.getElementById("mainContainer").addEventListener("click", function(e) {
+    //Used for exit out buttons on the streamer divs.
     if(e.target && e.target.matches("button.exitBtn") ){
       //Variable used to delete div container pertaining to button clicked.
-      var parentDiv = e.target.parentNode.parentNode.id;
+      var parentDiv = e.target.parentNode.parentNode.parentNode.id;
       //Deleting parent div on clicking the x button.
       deleteDiv(parentDiv);
     }
+    //Used to expand and show the streamer live stream on the page itself.
+    else if(e.target && e.target.matches("i.fa-caret-down") ){
+      //Variable used to determined video element to show appropriate streamer.
+      var parentId = e.target.parentNode.id;
+      console.log(parentId);
+      //Appending video to page.
+      showVideo(parentId);
+    }
+    else if(e.target && e.target.matches("i.fa-caret-up") ) {
+      var caret = e.target.id;
+      console.log(caret);
+      var iframe = "iframe" + caret.match(/\d+/)[0];
+      console.log(iframe);
+      deleteVideo(iframe);
+    }
+
   });
 }
 
+//deleteDiv() deletes the streamer container when the x button is clicked
+//x button is located on upper right corner of every streamer container.
 function deleteDiv(id){
-  console.log(id);
-  num--;
+  //Checks to see if the number of streamers shown is greater than one.
+  //If it is, the search bar will appear.
   if(num < 1){
     document.getElementById("searchBar").style.display = "none";
   }
+  num--;
   document.getElementById(id).remove();
 }
 
-function hideDiv(){
-
+//Following radios: show all streamers, show only online, show only offline.
+function allRadio(){
+  //Will loop through all streamer containers and display.
+  var wrappers = document.getElementsByClassName("wrapper");
+  for(var i = 0; i < wrappers.length; i++){
+    wrappers[i].style.display = "table";
+  }
 }
-
-function hideMultipleDivs(){
-
+//Show only online streamers.
+function onlineRadio(){
+  var mainDiv = document.getElementById("mainContainer")
+  var myParagraphs = mainDiv.getElementsByTagName("p");
+  //Will loop through myParagraphs array and will hide parent containers that are "Offline".
+  for(var i = 0; i < myParagraphs.length; i++){
+    if(myParagraphs[i].innerHTML == "Offline"){
+      myParagraphs[i].parentNode.parentNode.parentNode.style.display = "none";
+    }
+    else if(myParagraphs[i].innerHTML != "Offline" && myParagraphs[i].parentNode.parentNode.parentNode.style.display == "none"){
+      myParagraphs[i].parentNode.parentNode.parentNode.style.display = "table";
+    }
+  }
+}
+//Show only offline streamers.
+function offlineRadio(){
+  var mainDiv = document.getElementById("mainContainer");
+  var myParagraphs = mainDiv.getElementsByTagName("p");
+  //Will loop through myParagraphs and hide parent containers that are "Online".
+  for(var i = 0; i < myParagraphs.length; i++){
+    if(myParagraphs[i].innerHTML != "Offline"){
+      myParagraphs[i].parentNode.parentNode.parentNode.style.display = "none";
+    }
+    else if(myParagraphs[i].innerHTML == "Offline" && myParagraphs[i].parentNode.parentNode.parentNode.style.display == "none"){
+      myParagraphs[i].parentNode.parentNode.parentNode.style.display = "table";
+    }
+  }
 }
 
 
